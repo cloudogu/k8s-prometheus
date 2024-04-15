b02360de1b0918c9ec8594ba610c549bddf6268d
Nico Franzeck <nico.franzeck@cloudogu.com>
2024-04-15 21:36:19 +0200

    check main method by evaluating ports

--- main_test.go
+++ main_test.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/cloudogu/k8s-prometheus/auth/configuration"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"log/slog"
+	"net"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 	"testing"
 	"time"
 )
 
 func Test_main(t *testing.T) {
+	const (
+		defaultAuthPort = 8086
+		defaultSrvPort  = 8087
+	)
+
 	t.Run("should start servers", func(t *testing.T) {
 
 		err := os.Setenv("WEB_CONFIG_FILE", "/web-config.file")
@@ -28,22 +36,87 @@ func Test_main(t *testing.T) {
 		// Register SIGINT to the signal channel
 		signal.Notify(sigChan, syscall.SIGINT)
 
-		// Create a timer for 5 seconds
-		timer := time.NewTimer(5 * time.Second)
+		go func() {
+			assert.NotPanics(t, main)
+		}()
 
-		assert.NotPanics(t, func() {
-			go main()
-		})
+		time.Sleep(100 * time.Millisecond)
 
-		// Wait for either the timer to expire or SIGINT to be received
-		select {
-		case <-timer.C:
-			// Timer expired, send SIGINT signal to the process
-			sendSignal(syscall.SIGINT)
-		case <-sigChan:
-			// SIGINT received, do nothing (the program will exit gracefully)
-		}
+		// assert correct start of the server
+		var wg sync.WaitGroup
+
+		wg.Add(2)
+		go func() {
+			defer wg.Done()
+			assert.True(t, checkPort(defaultAuthPort, false))
+		}()
+
+		go func() {
+			defer wg.Done()
+			assert.True(t, checkPort(defaultSrvPort, false))
+		}()
+
+		wg.Wait()
+
+		sendSignal(syscall.SIGINT)
+
+		// assert graceful shutdown
+		wg.Add(2)
+		go func() {
+			defer wg.Done()
+			assert.True(t, checkPort(defaultAuthPort, true))
+		}()
+
+		go func() {
+			defer wg.Done()
+			assert.True(t, checkPort(defaultSrvPort, true))
+		}()
+
+		wg.Wait()
 	})
+}
+
+func isPortAvailable(port int) bool {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		// Port is not available
+		return false
+	}
+
+	defer func() {
+		_ = listener.Close()
+	}()
+
+	// Port is available
+	return true
+}
+
+func checkPort(port int, available bool) bool {
+	ctxWithCancel, cancel := context.WithTimeout(context.TODO(), 3*time.Second)
+	defer cancel()
+
+	resultChan := make(chan bool)
+	go func() {
+	loop:
+		for {
+			select {
+			case <-ctxWithCancel.Done():
+				break loop
+			default:
+				if result := isPortAvailable(port); result == available {
+					resultChan <- result
+				}
+			}
+
+		}
+	}()
+
+	select {
+	case <-ctxWithCancel.Done():
+		return false
+	case <-resultChan:
+		return true
+	}
 }
 
 func Test_configureLogger(t *testing.T) {
